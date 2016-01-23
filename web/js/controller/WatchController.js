8afc7673061726e2d6c28a694bf76129642d9ee6
mdronamr <mdronamr@douglas.co.us>
2016-01-22 22:10:16 -0700

    Test

---
+++ web/js/controller/WatchController.js
@@ -0,0 +1,19 @@
+'use strict';
+
+App.controller('watchController', function($scope, AccountService) {
+    var watchController = this;
+    watchController.matchedAccounts = [];
+    $scope.testName = "";
+
+    $scope.$watch("testName", function(newValue, oldValue) {
+        if ($scope.testName.length > 0) {
+            $scope.greeting = "Greetings " + $scope.testName;
+            $scope.matchedAccounts = AccountService.fetchMatchedAccounts(newValue);
+            AccountService.fetchMatchedAccounts(newValue).then(function(data) {
+                watchController.matchedAccounts = data;
+                $scope.matchedAccounts = data;
+                console.log('watchController.matchedAccounts: ', watchController.matchedAccounts);
+            });
+        }
+    });
+});
