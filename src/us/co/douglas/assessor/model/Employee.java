dede9642ae6b5b33461839a2c2a79f66fe8570c2
mdronamr <mdronamr@douglas.co.us>
2016-02-23 13:33:24 -0700

    Test

---
+++ src/us/co/douglas/assessor/model/Employee.java
@@ -0,0 +1,55 @@
+package us.co.douglas.assessor.model;
+
+/**
+ * Created by mdronamr on 2/23/16.
+ */
+import org.codehaus.jackson.annotate.JsonProperty;
+
+public class Employee {
+    private String employeeId;
+    private String type;
+    private String firstName;
+    private String lastName;
+
+    @JsonProperty("_id")
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    @JsonProperty("first-name")
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @JsonProperty("type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @JsonProperty("last-name")
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee [employeeId=" + employeeId + ", type=" + type
+                + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+    }
+}
